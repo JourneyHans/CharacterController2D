b4b583aabcf9c96cc2f9c0e82c52eebb82e52958
JourneyHans <2749628228@qq.com>
2018-10-12 21:43:40 +0800

    no message

--- Assets/CharacterController2D.cs
+++ Assets/CharacterController2D.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class CharacterController2D : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;       // 移动速度
+    [SerializeField] private float xSpeed;      // 水平方向速度
+    [SerializeField] private float gravity;         // 重力
 
     private SpriteRenderer _sprite;
+    private Rigidbody2D _rigidbody;
+
+    private Vector2 velocity;       // 速度
 
     void Awake()
     {
         _sprite = GetComponent<SpriteRenderer>();
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Start()
     {
+//        if (Physics2D.Raycast(this.transform.position, Vector2.down, 10.0f, 1 << LayerMask.NameToLayer("Collider")))
+//        {
+//            string colliderName = Physics2D.Raycast(this.transform.position, Vector2.down, 10.0f, 1 << LayerMask.NameToLayer("Collider")).collider.gameObject.name;
+//            Debug.Log("Collider " + colliderName + " is under the CubeUp");
+//        }
+//        else
+//        {
+//            Debug.Log("No collider is under the CubeUp");
+//        }
     }
 
     void Update()
@@ -24,14 +38,31 @@ public class CharacterController2D : MonoBehaviour
 
     void FixedUpdate()
     {
+        // 移动前的位置
+        Vector2 currentPos = _rigidbody.position;
+
+        // 处理水平方向
         int xInput = (int)Input.GetAxisRaw("Horizontal");
-        float xPos = xInput * moveSpeed * Time.deltaTime;
-        Vector2 result = new Vector2(xPos, 0);
+        velocity.x = xInput * xSpeed;
+
+        // 处理垂直方向
+        velocity.y = -gravity;
+
+//        Debug.DrawLine(_rigidbody.position, _rigidbody.position + new Vector2(0, -1f) * 1f, Color.blue);
+//        RaycastHit2D hit = Physics2D.Raycast(_rigidbody.position, new Vector2(0, -1f), 1f, 1 << LayerMask.NameToLayer("Platform"));
+//        if (hit)
+//        {
+//            Debug.Log("Collider: " + hit.collider.name);
+//        }
+
+        // 最终位置
+        Vector2 result = velocity * Time.deltaTime + currentPos;
 
         // 处理转向
         HandleTurn(xInput);
 
-        transform.Translate(result);
+        _rigidbody.MovePosition(result);
+//        transform.Translate(result);
     }
 
     // 处理转向
